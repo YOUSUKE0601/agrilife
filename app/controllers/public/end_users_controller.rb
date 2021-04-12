7d779503706018367c9804fa7ab9ed5fdee10049
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-04-12 12:00:00 +0000

    [Update]マイページレイアウト

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,6 +1,12 @@
 class Public::EndUsersController < ApplicationController
   
   def my_page
+    @end_user = current_end_user
   end
+  
+  def edit
+    @end_user = current_end_user
+  end
+  
   
 end
