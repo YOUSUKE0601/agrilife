9899796e419e6560764f8d224be591b2f24085aa
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-04-15 13:07:17 +0000

    [Add]検索機能

--- app/controllers/public/farmers_controller.rb
+++ app/controllers/public/farmers_controller.rb
@@ -1,6 +1,11 @@
 class Public::FarmersController < ApplicationController
 
-  def 
+  def serch 
+    @q = Farmer.ransack(params[:q])
+    @farmers = @q.result.order(:farm_name)
+  end
+  
+  def show
   end
 
 end
