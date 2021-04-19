f96ebc955b0f8308f36c4a566ce16f4dde33c09e
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-04-19 12:22:28 +0000

    [Add]フォロー機能非同期通期化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,17 +1,15 @@
 class Public::RelationshipsController < ApplicationController
   
   def create
-    farmer = Farmer.find(params[:farmer_id])
-    relationship = current_end_user.relationships.new(farmer_id: farmer.id)
-    relationship.save
-    redirect_to public_farmer_path(farmer)
+    @farmer = Farmer.find(params[:farmer_id])
+    @relationship = current_end_user.relationships.new(farmer_id: @farmer.id)
+    @relationship.save
   end
   
   def destroy
-    farmer = Farmer.find(params[:farmer_id])
-    relationship = current_end_user.relationships.find_by(farmer_id: farmer.id)
-    relationship.destroy
-    redirect_to public_farmer_path(farmer)
+    @farmer = Farmer.find(params[:farmer_id])
+    @relationship = current_end_user.relationships.find_by(farmer_id: @farmer.id)
+    @relationship.destroy
   end
   
 end
