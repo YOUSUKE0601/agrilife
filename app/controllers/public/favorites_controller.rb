cd361f3fed2fb4fa3b717ae72a40636f29739db7
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-04-19 11:05:27 +0000

    [Add]いいね機能・コメント機能を非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,17 @@
 class Public::FavoritesController < ApplicationController
   
   def create
-    topic = Topic.find(params[:topic_id])
-    favorite = current_end_user.favorites.new(topic_id: topic.id)
+    @topic = Topic.find(params[:topic_id])
+    favorite = current_end_user.favorites.new(topic_id: @topic.id)
     favorite.save
-    redirect_to public_topic_path(topic)
+    # app/views/public/favorites/create.js.erbを参照する
   end
   
   def destroy
-    topic = Topic.find(params[:topic_id])
-    favorite = current_end_user.favorites.find_by(topic_id: topic.id)
+    @topic = Topic.find(params[:topic_id])
+    favorite = current_end_user.favorites.find_by(topic_id: @topic.id)
     favorite.destroy
-    redirect_to public_topic_path(topic)
+    # app/views/public/favorites/destroy.js.erbを参照する
   end
   
   
--- app/controllers/public/topic_comments_controller.rb
+++ app/controllers/public/topic_comments_controller.rb
@@ -1,16 +1,18 @@
 class Public::TopicCommentsController < ApplicationController
 
   def create
-    topic = Topic.find(params[:topic_id])
-    comment = current_end_user.topic_comments.new(topic_comment_params)
-    comment.topic_id = topic.id
-    comment.save
-    redirect_to public_topic_path(topic)
+    @topic = Topic.find(params[:topic_id])
+    @comment = current_end_user.topic_comments.new(topic_comment_params)
+    @comment.topic_id = @topic.id
+    @comment.save
+    # app/views/public/topic_comments/create.js.erbを参照する
   end
 
   def destroy
-    TopicComment.find_by(id: params[:id], topic_id: params[:topic_id]).destroy
-    redirect_to public_topic_path(params[:topic_id])
+    @topic = Topic.find(params[:topic_id])
+    @comment = TopicComment.find_by(id: params[:id], topic_id: @topic)
+    @comment.destroy
+    # app/views/public/topic_comments/destroy.js.erbを参照する
   end
 
   private
